294061e327d760963fa04d2088a31f9b2c67c92e
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-08-05 06:04:53 +0530

    Update __nightmode.py

--- innexiaBot/modules/__nightmode.py
+++ innexiaBot/modules/__nightmode.py
@@ -151,7 +151,7 @@ async def job_open():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), " `06:00 Am, Group Is Opening.\n**Powered By Patricia**` "
+              int(pro.chat_id), "`06:00 Am, Group Is Opening.`\n Powered by @PATRICIA_ROBOT"
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(
