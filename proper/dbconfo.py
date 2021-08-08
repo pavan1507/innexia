087196d544eb183f69853781a2296165ed0c9920
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-08-08 10:05:21 +0530

    Update dbconfo.py

--- proper/dbconfo.py
+++ proper/dbconfo.py
@@ -1,11 +1,17 @@
+
 import os
 
-class Config(object):  
-   
+class Config(object):
+    # get it from my.telegram.org
+    APP_ID = os.environ.get("APP_ID", "3546656")
+    API_HASH = os.environ.get("API_HASH", "48b79c54af688f05c350161bddea414c")
+    # Get it from @botfather
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1728730929:AAESo9G0f62qLCXJnOWcKzp5Vblmrktivd4")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "PATRICIA_ROBOT")
     # Leave this defualt
     SESSION_NAME = os.environ.get("SESSION_NAME", "JV_CaptchaBot")
     # get it from https://cloud.mongodb.com 
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://Evil:NRb0LXgSBdao6tHB@cluster0.phtr6.mongodb.net/EvilretryWrites=true&w=majority")
+    # Ask in https://t.me/JV_Community
     API_TOKEN = os.environ.get("API_TOKEN", "dontsellme_iamfreeapi")
-    # Sudo users(goto @JVToolsBot and send /id to get your id)
-   
+    
